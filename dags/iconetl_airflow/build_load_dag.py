17fb4a13d50709c8d09740458fa50aeb0a9b3b78
Richard Mah <dick.mah@gmail.com>
2020-10-02 16:05:35 -0400

    adjust bq load dag

--- dags/iconetl_airflow/build_load_dag.py
+++ dags/iconetl_airflow/build_load_dag.py
@@ -21,14 +21,14 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 
 def build_load_dag(
-    dag_id,
-    output_bucket,
-    destination_dataset_project_id,
-    chain='icon',
-    notification_emails=None,
-    load_start_date=datetime(2018, 1, 24),
-    schedule_interval='0 0 * * *',
-    load_all_partitions=True
+        dag_id,
+        output_bucket,
+        destination_dataset_project_id,
+        chain='icon',
+        notification_emails=None,
+        load_start_date=datetime(2018, 1, 24),
+        schedule_interval='0 0 * * *',
+        load_all_partitions=True
 ):
     # The following datasets must be created in BigQuery:
     # - crypto_{chain}_raw
@@ -129,174 +129,18 @@ def build_load_dag(
         wait_sensor >> load_operator
         return load_operator
 
-    def add_enrich_tasks(task, time_partitioning_field='block_timestamp', dependencies=None, always_load_all_partitions=False):
-        def enrich_task(ds, **kwargs):
-            template_context = kwargs.copy()
-            template_context['ds'] = ds
-            template_context['params'] = environment
-
-            client = bigquery.Client()
-
-            # Need to use a temporary table because bq query sets field modes to NULLABLE and descriptions to null
-            # when writeDisposition is WRITE_TRUNCATE
-
-            # Create a temporary table
-            temp_table_name = '{task}_{milliseconds}'.format(task=task, milliseconds=int(round(time.time() * 1000)))
-            temp_table_ref = client.dataset(dataset_name_temp).table(temp_table_name)
-
-            schema_path = os.path.join(dags_folder, 'resources/stages/enrich/schemas/{task}.json'.format(task=task))
-            schema = read_bigquery_schema_from_file(schema_path)
-            table = bigquery.Table(temp_table_ref, schema=schema)
-
-            description_path = os.path.join(
-                dags_folder, 'resources/stages/enrich/descriptions/{task}.txt'.format(task=task))
-            table.description = read_file(description_path)
-            if time_partitioning_field is not None:
-                table.time_partitioning = TimePartitioning(field=time_partitioning_field)
-            logging.info('Creating table: ' + json.dumps(table.to_api_repr()))
-            table = client.create_table(table)
-            assert table.table_id == temp_table_name
-
-            # Query from raw to temporary table
-            query_job_config = bigquery.QueryJobConfig()
-            # Finishes faster, query limit for concurrent interactive queries is 50
-            query_job_config.priority = bigquery.QueryPriority.INTERACTIVE
-            query_job_config.destination = temp_table_ref
-
-            sql_path = os.path.join(dags_folder, 'resources/stages/enrich/sqls/{task}.sql'.format(task=task))
-            sql_template = read_file(sql_path)
-            sql = kwargs['task'].render_template('', sql_template, template_context)
-            print('Enrichment sql:')
-            print(sql)
-
-            query_job = client.query(sql, location='US', job_config=query_job_config)
-            submit_bigquery_job(query_job, query_job_config)
-            assert query_job.state == 'DONE'
-
-            if load_all_partitions or always_load_all_partitions:
-                # Copy temporary table to destination
-                copy_job_config = bigquery.CopyJobConfig()
-                copy_job_config.write_disposition = 'WRITE_TRUNCATE'
-                dest_table_name = '{task}'.format(task=task)
-                dest_table_ref = client.dataset(dataset_name, project=destination_dataset_project_id).table(dest_table_name)
-                copy_job = client.copy_table(temp_table_ref, dest_table_ref, location='US', job_config=copy_job_config)
-                submit_bigquery_job(copy_job, copy_job_config)
-                assert copy_job.state == 'DONE'
-            else:
-                # Merge
-                # https://cloud.google.com/bigquery/docs/reference/standard-sql/dml-syntax#merge_statement
-                merge_job_config = bigquery.QueryJobConfig()
-                # Finishes faster, query limit for concurrent interactive queries is 50
-                merge_job_config.priority = bigquery.QueryPriority.INTERACTIVE
-
-                merge_sql_path = os.path.join(
-                    dags_folder, 'resources/stages/enrich/sqls/merge/merge_{task}.sql'.format(task=task))
-                merge_sql_template = read_file(merge_sql_path)
-
-                merge_template_context = template_context.copy()
-                merge_template_context['params']['source_table'] = temp_table_name
-                merge_template_context['params']['destination_dataset_project_id'] = destination_dataset_project_id
-                merge_template_context['params']['destination_dataset_name'] = dataset_name
-                merge_sql = kwargs['task'].render_template('', merge_sql_template, merge_template_context)
-                print('Merge sql:')
-                print(merge_sql)
-                merge_job = client.query(merge_sql, location='US', job_config=merge_job_config)
-                submit_bigquery_job(merge_job, merge_job_config)
-                assert merge_job.state == 'DONE'
-
-            # Delete temp table
-            client.delete_table(temp_table_ref)
-
-        enrich_operator = PythonOperator(
-            task_id='enrich_{task}'.format(task=task),
-            python_callable=enrich_task,
-            provide_context=True,
-            execution_timeout=timedelta(minutes=60),
-            dag=dag
-        )
-
-        if dependencies is not None and len(dependencies) > 0:
-            for dependency in dependencies:
-                dependency >> enrich_operator
-        return enrich_operator
-
-    def add_verify_tasks(task, dependencies=None):
-        # The queries in verify/sqls will fail when the condition is not met
-        # Have to use this trick since the Python 2 version of BigQueryCheckOperator doesn't support standard SQL
-        # and legacy SQL can't be used to query partitioned tables.
-        sql_path = os.path.join(dags_folder, 'resources/stages/verify/sqls/{task}.sql'.format(task=task))
-        sql = read_file(sql_path)
-        verify_task = BigQueryOperator(
-            task_id='verify_{task}'.format(task=task),
-            bql=sql,
-            params=environment,
-            use_legacy_sql=False,
-            dag=dag)
-        if dependencies is not None and len(dependencies) > 0:
-            for dependency in dependencies:
-                dependency >> verify_task
-        return verify_task
-
     load_blocks_task = add_load_tasks('blocks', 'csv')
     load_transactions_task = add_load_tasks('transactions', 'csv')
     load_receipts_task = add_load_tasks('receipts', 'csv')
     load_logs_task = add_load_tasks('logs', 'json')
-    load_contracts_task = add_load_tasks('contracts', 'json')
-    load_tokens_task = add_load_tasks('tokens', 'csv', allow_quoted_newlines=True)
-    load_token_transfers_task = add_load_tasks('token_transfers', 'csv')
-    load_traces_task = add_load_tasks('traces', 'csv')
-
-    enrich_blocks_task = add_enrich_tasks(
-        'blocks', time_partitioning_field='timestamp', dependencies=[load_blocks_task])
-    enrich_transactions_task = add_enrich_tasks(
-        'transactions', dependencies=[load_blocks_task, load_transactions_task, load_receipts_task])
-    enrich_logs_task = add_enrich_tasks(
-        'logs', dependencies=[load_blocks_task, load_logs_task])
-    enrich_token_transfers_task = add_enrich_tasks(
-        'token_transfers', dependencies=[load_blocks_task, load_token_transfers_task])
-    enrich_traces_task = add_enrich_tasks(
-        'traces', dependencies=[load_blocks_task, load_traces_task])
-    enrich_contracts_task = add_enrich_tasks(
-        'contracts', dependencies=[load_blocks_task, load_contracts_task])
-    enrich_tokens_task = add_enrich_tasks(
-        'tokens', dependencies=[load_blocks_task, load_tokens_task])
-
-    calculate_balances_task = add_enrich_tasks(
-        'balances', dependencies=[enrich_blocks_task, enrich_transactions_task, enrich_traces_task],
-        time_partitioning_field=None, always_load_all_partitions=True)
-
-    verify_blocks_count_task = add_verify_tasks('blocks_count', [enrich_blocks_task])
-    verify_blocks_have_latest_task = add_verify_tasks('blocks_have_latest', [enrich_blocks_task])
-    verify_transactions_count_task = add_verify_tasks('transactions_count',
-                                                      [enrich_blocks_task, enrich_transactions_task])
-    verify_transactions_have_latest_task = add_verify_tasks('transactions_have_latest', [enrich_transactions_task])
-    verify_logs_have_latest_task = add_verify_tasks('logs_have_latest', [enrich_logs_task])
-    verify_token_transfers_have_latest_task = add_verify_tasks('token_transfers_have_latest',
-                                                               [enrich_token_transfers_task])
-    verify_traces_blocks_count_task = add_verify_tasks('traces_blocks_count', [enrich_blocks_task, enrich_traces_task])
-    verify_traces_transactions_count_task = add_verify_tasks(
-        'traces_transactions_count', [enrich_transactions_task, enrich_traces_task])
-    verify_traces_contracts_count_task = add_verify_tasks(
-        'traces_contracts_count', [enrich_transactions_task, enrich_traces_task, enrich_contracts_task])
 
     if notification_emails and len(notification_emails) > 0:
         send_email_task = EmailOperator(
             task_id='send_email',
             to=[email.strip() for email in notification_emails.split(',')],
-            subject='Ethereum ETL Airflow Load DAG Succeeded',
-            html_content='Ethereum ETL Airflow Load DAG Succeeded - {}'.format(chain),
+            subject='ICON ETL Airflow Load DAG Succeeded',
+            html_content='ICON ETL Airflow Load DAG Succeeded - {}'.format(chain),
             dag=dag
         )
-        verify_blocks_count_task >> send_email_task
-        verify_blocks_have_latest_task >> send_email_task
-        verify_transactions_count_task >> send_email_task
-        verify_transactions_have_latest_task >> send_email_task
-        verify_logs_have_latest_task >> send_email_task
-        verify_token_transfers_have_latest_task >> send_email_task
-        verify_traces_blocks_count_task >> send_email_task
-        verify_traces_transactions_count_task >> send_email_task
-        verify_traces_contracts_count_task >> send_email_task
-        enrich_tokens_task >> send_email_task
-        calculate_balances_task >> send_email_task
 
     return dag
